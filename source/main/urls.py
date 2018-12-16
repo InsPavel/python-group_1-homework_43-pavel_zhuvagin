2b96c7d9086de84783f5c62abe4a875cc8afc690
pavel <zhpv07@gmail.com>
2018-12-16 01:48:39 -0500

    Added url

--- source/main/urls.py
+++ source/main/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webapp.views import ArticleListView, ArticleDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', ArticleListView.as_view(), name='article_list'),
+    path('article/<int:pk>', ArticleDetailView.as_view(), name='article_detail')
+
 ]
