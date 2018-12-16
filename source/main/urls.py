c3b3a0a0f170c0b4f4e5b8e298346a8963e9ac34
pavel <zhpv07@gmail.com>
2018-12-16 10:55:59 -0500

    Added user_list in urls.py

--- source/main/urls.py
+++ source/main/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from webapp.views import ArticleListView, ArticleDetailView
+from webapp.views import ArticleListView, ArticleDetailView, UserListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', ArticleListView.as_view(), name='article_list'),
-    path('article/<int:pk>', ArticleDetailView.as_view(), name='article_detail')
+    path('article/<int:pk>', ArticleDetailView.as_view(), name='article_detail'),
+    path('user', UserListView.as_view(), name='user_list'),
 
 ]
