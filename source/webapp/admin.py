a283053b822c607cfddd05f4c8a2bc14547dafe4
pavel <zhpv07@gmail.com>
2018-12-15 23:47:13 -0500

    Added User and Article

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from webapp.models import User, Article
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    filter_horizontal = ('favorites',)
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Article)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Имя")
+    favorites = models.ManyToManyField('Article', blank=True, related_name='favored_by', verbose_name='Избранное')
+
+    def __str__(self):
+        return self.name
+
+class Article(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
+    text = models.TextField(max_length=2000, verbose_name='Текст')
+    author = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name='Автор')
+
+    def __str__(self):
+        return '%s, %s' % (self.title, self.author)
